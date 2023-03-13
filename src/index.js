367872ba22a2a152f279ef0607cfbb849619acf6
sachitrana2001 <ranasachit2001jan@gmail.com>
2023-03-13 12:53:59 +0530

    add redux files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { store } from './App/store'
+import { Provider } from 'react-redux'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
